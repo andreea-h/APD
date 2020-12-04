13a9edda1eb962d94bc9f6e89a5c9eb6ff87fb1c
andreea-h <andreeahorovei@yahoo.com>
2020-12-05 00:31:29 +0200

    task strinct_x_car

--- Tema2/src/com/apd/tema2/intersections/SimpleStrict_X_CarRoundabout.java
+++ Tema2/src/com/apd/tema2/intersections/SimpleStrict_X_CarRoundabout.java
@@ -7,7 +7,7 @@ import com.apd.tema2.entities.Intersection;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
-
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**intersectie in care pot intra x masini din fiecare directie la un moment de timp**/
 public class SimpleStrict_X_CarRoundabout implements Intersection {
@@ -18,8 +18,8 @@ public class SimpleStrict_X_CarRoundabout implements Intersection {
     Semaphore[] semaphores; /**tine controlul asupra numarului de masini care pot intra in intersectie din fiecare directie**/
     CyclicBarrier barrier1;
     Semaphore exitSempahore;
-    int a = 0;
-    Object o = new Object();
+    Object obj = new Object();
+    AtomicInteger nrCars = new AtomicInteger(0);
 
     public void action(Car car) {
         /**inainte de a trece mai departe trebuie ca toate masinile sa faca reached**/
@@ -30,18 +30,24 @@ public class SimpleStrict_X_CarRoundabout implements Intersection {
         catch(InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
-
-        /**inainte de a face selectia unei masini trebuie ca toate masinile din runda precedenta sa fie iesite din intersectie**/
         /*
         try {
-            exitSempahore.acquire();
+            exitSempahore.acquire(x * noDirections);
         }
         catch(InterruptedException e) {
             e.printStackTrace();
         }
         */
+        /**inainte de a face selectia unei masini trebuie ca toate masinile din runda precedenta sa fie iesite din intersectie**/
         try {
             semaphores[car.getStartDirection()].acquire();
+            synchronized (obj) {
+                // System.out.println("asteptare nr masini: " + nrCars.get());
+                //while (nrCars.get() != x * noDirections && nrCars.get() != 0)
+                if (nrCars.get() != 0) {
+                    obj.wait();
+                }
+            }
             System.out.println("Car " + car.getId() + " was selected to enter the roundabout from lane " + car.getStartDirection());
         }
         catch(InterruptedException e) {
@@ -57,13 +63,21 @@ public class SimpleStrict_X_CarRoundabout implements Intersection {
             e.printStackTrace();
         }
 
-        System.out.println("Car " + car.getId() + " has exited the roundabout after " + (int)T/1000 + " seconds");
-        /**inainte de a trece mai departe trebuie ca toate masinile sa paraseasca sensul giratoriu**/
+        synchronized (obj) {
+            System.out.println("Car " + car.getId() + " has exited the roundabout after " + (int)T/1000 + " seconds");
+            /**inainte de a trece mai departe trebuie ca toate masinile sa paraseasca sensul giratoriu**/
 
-        /**permite unei alte masini orientata pe aceeasi directie sa intre in intersectie**/
-        semaphores[car.getStartDirection()].release();
+            /**permite unei alte masini orientata pe aceeasi directie sa intre in intersectie**/
+            semaphores[car.getStartDirection()].release();
 
-       // exitSempahore.release(); /**numara cate masini sunt in intersectie**/
+            nrCars.getAndAdd(1);
+            //System.out.println("Nr de masini detectat dupa ce a iesit masina " + car.getId() + " este " + nrCars);
+            if (nrCars.get() == x * noDirections) {
+               // System.out.println("Nr de masini detectat dupa ce a iesit masina " + car.getId() + " este " + nrCars);
+                obj.notify();
+                nrCars.set(0);
+            }
+        }
     }
 
     public void setNoDirections(int noDirections) {
